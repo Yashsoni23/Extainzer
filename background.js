85c235d7e05f7b58d266b8944557a00a50f323da
Yash simejiya <yashsoni48678@gmail.com>
2024-01-31 18:03:07 +0530

    samuraizer extension working code

---
+++ background.js
@@ -0,0 +1,54 @@
+// content.js
+
+async function summarize(content) {
+  try {
+    const requestBody = {
+      min_length: "62",
+      text_to_summarize: content,
+    };
+
+    const response = await fetch("https://samurizer.adaptable.app/summarize", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error summarizing content:", error.message);
+    throw error.message; // Rethrow the error for handling in the caller function
+  }
+}
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  // Check if the message action is 'scrapedContent'
+  if (message.action === "scrapedContent") {
+    // Perform summarization logic
+    const scrapedContent = message.content;
+
+    // Example summarization logic (you can replace this with your own summarization algorithm)
+    summarize(scrapedContent)
+      .then((summarizedContent) => {
+        console.log(summarizedContent);
+        // Send the summarized content back to the content script
+        sendResponse({
+          action: "summarizedContent",
+          content: summarizedContent.result,
+        });
+      })
+      .catch((error) => {
+        console.error("Error during summarization:", error);
+        // Send an error response back to the content script
+        sendResponse({ action: "summarizationError", error: error.message });
+      });
+
+    // Return true to indicate that the response will be sent asynchronously
+    return true;
+  }
+});
---
+++ popup.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () =>
+  alert("Are you sure you want to continue ?)", false)
+);
